ef26c755f5ca1ed702582a4edbd8e9fe7ba6c291
Paul Thomas <paulthomas2107@gmail.com>
2023-03-15 13:09:32 +0000

    More

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import Header from './components/Header';
+import './App.css';
+import './scss/main.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Header></Header>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ vite-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <div>Hello Paul ! {import.meta.env.VITE_API_URL}</div>;
+};
+
+export default Header;
